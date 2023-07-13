23be1fdc7ba48381ba5b679921362fa628516bab
sahil-gusain <sahilgusainddn@gmail.com>
2023-07-13 22:43:32 +0530

    ml portion finished

---
+++ mainTest.py
@@ -0,0 +1,24 @@
+import cv2
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+
+model=load_model('new_model.h5')
+
+image=cv2.imread('D:\\Brain Tumor detection system\\testing\\pred0.jpg')
+
+img=Image.fromarray(image)
+
+img=img.resize((64,64))
+
+img=np.array(img)
+
+input_img=np.expand_dims(img, axis=0)
+
+result=predict_x=model.predict(input_img)
+classes_x=np.argmax(result,axis=1)
+print(classes_x)
+
+
+
+
